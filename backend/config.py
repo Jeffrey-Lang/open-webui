e498b5f162ab8f4e650f82284af19ecb2b698331
Jeffrey Lang <jeffrey@planningsagenda.nl>
2024-08-03 22:45:25 +0200

    fix

--- backend/config.py
+++ backend/config.py
@@ -87,9 +87,7 @@ class EndpointFilter(logging.Filter):
 logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Planningagenda AI")
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
